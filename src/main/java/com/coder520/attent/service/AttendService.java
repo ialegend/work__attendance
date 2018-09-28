d6c6b2163c1518f92164ee8aacecc4a1a998e70c
IAlegend <1354288573@qq.com>
2018-09-28 17:52:45 +0800

    activity工作流

--- src/main/java/com/coder520/attent/service/AttendService.java
+++ src/main/java/com/coder520/attent/service/AttendService.java
@@ -5,7 +5,7 @@ import com.coder520.attent.vo.QueryCondition;
 import com.coder520.common.page.PageQueryBean;
 
 public interface AttendService {
-   void signAttend(Attend attend);
+    void signAttend(Attend attend);
 
     PageQueryBean listAttend(QueryCondition condition);
 
